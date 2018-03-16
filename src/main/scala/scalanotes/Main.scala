13e385dbc275c539ebe71e030ec69c7959c2f0a6
david dinh <david.dinh@lookout.com>
2018-03-15 20:30:19 -0700

    update for ch.9

--- src/main/scala/scalanotes/Main.scala
+++ src/main/scala/scalanotes/Main.scala
@@ -14,7 +14,7 @@ import cats.syntax.traverse._ // for traverse
 import cats.syntax.functor._ // for map
 import cats.{ Id, Applicative }
 
-object Main extends App {
+object Ch8TestingAsyncCode {
   // F[_] is a type constructor that we can bind to Future or Id
   trait UptimeClient[F[_]] {
     def getUptime(hostname: String): F[Int]
@@ -36,6 +36,75 @@ object Main extends App {
     val actual = service.getTotalUptime(hosts.keys.toList)
     val expected = hosts.values.sum
     assert(actual == expected)
+  }
+}
+
+/**
+ * Ch.9 Case Study: Map Reduce
+ */
+
+import cats.{ Monoid }
+import cats.instances.int._ // for Monoid
+import cats.instances.string._ // for Monoid
+import cats.syntax.semigroup._ // for |+|
+
+object Ch9Notes {
+  // FYI: Cat's TraversableOnce trait:
+  // A template trait for collections which can be traversed either once only
+  // or one or more times.
+  //
+  // foldLeft, foldRight, and associated methods:
+  //
+
+  // Q: how does foreach work in this trait?
+  //
+  // def foreach[U](f: A => U): Unit
+
+  // Q: how does self work in this trait?
+  // Q: what is ::= ?
+  //
+  // protected[this] def reversed = {
+  //   var elems: List[A] = Nil
+  //   self foreach (elems ::= _)
+  //   elems
+  // }
+
+  // def foldRight[B](z: B)(op: (A, B) => B): B =
+  //   reversed.foldLeft(z)((x, y) => op(y, x))
+
+  // def foldLeft[B](z: B)(op: (B, A) => B): B = {
+  //   var result = z
+  //   this foreach (x => result = op(result, x))
+  //   result
+  // }
+
+}
+
+object Main extends App {
+
+  /**
+   * Single-threaded map-reduce function.
+   * Maps `f` over `sequence` and reduces using a `Monoid[B]`.
+   *
+   * Parallel fold will yield the correct results if:
+   *  - the reducer function is associative
+   *  - we seed the computation with the identity of this function.
+   */
+
+  def foldMap2[A, B: Monoid](as: Vector[A])(f: A => B): B =
+    as.map(_ + _)
+
+  def foldMap[A, B: Monoid](as: Vector[A])(f: A => B): B =
+    as.foldLeft(Monoid[B].empty)(_ |+| f(_))
+
+  def testFoldMap() = {
+    assert(
+      foldMap(Vector(1, 2, 3))(_.toString + "! ") == "1! 2! 3!"
+    )
+
+    assert(
+      foldMap("Hello world!".toVector)(_.toString.toUpperCase) == "HELLO WORLD!"
+    )
   }
 }
 
