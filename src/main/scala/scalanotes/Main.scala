fdb5bacd6711543bd43a01a22fc6591d2904b19a
david dinh <david.dinh@lookout.com>
2018-03-15 19:10:36 -0700

    update for ch.8

--- src/main/scala/scalanotes/Main.scala
+++ src/main/scala/scalanotes/Main.scala
@@ -4,22 +4,38 @@ package scalanotes
  * Ch.8 Case Study: Testing Asynchronous Code
  */
 
-import scala.concurrent.Future
-import scala.concurrent.ExecutionContext.Implicits.global
+//import scala.concurrent.Future
+//import scala.concurrent.ExecutionContext.Implicits.global
+import scala.language.higherKinds
 
-import cats.instances.future._ // for Applicative
+//import cats.instances.future._ // for Applicative
 import cats.instances.list._ // for Traverse
 import cats.syntax.traverse._ // for traverse
+import cats.syntax.functor._ // for map
+import cats.{ Id, Applicative }
 
 object Main extends App {
-  println("Hello Cats!")
+  // F[_] is a type constructor that we can bind to Future or Id
+  trait UptimeClient[F[_]] {
+    def getUptime(hostname: String): F[Int]
+  }
+
+  class UptimeService[F[_]: Applicative](client: UptimeClient[F]) {
+    def getTotalUptime(hostnames: List[String]): F[Int] =
+      hostnames.traverse(client.getUptime).map(_.sum)
+  }
+
+  class TestUptimeClient(hosts: Map[String, Int]) extends UptimeClient[Id] {
+    def getUptime(hostname: String): Id[Int] = hosts.getOrElse(hostname, 0)
+  }
+
+  def testTotalUptime() = {
+    val hosts = Map("host1" -> 10, "host2" -> 6)
+    val client = new TestUptimeClient(hosts)
+    val service = new UptimeService(client)
+    val actual = service.getTotalUptime(hosts.keys.toList)
+    val expected = hosts.values.sum
+    assert(actual == expected)
+  }
 }
 
-trait UptimeClient {
-  def getUptime(hostname: String): Future[Int]
-}
-
-class UptimeService(client: UptimeClient) {
-  def getTotalUptime(hostnames: List[String]): Future[Int] =
-    hostnames.traverse(client.getUptime).map(_.sum)
-}
